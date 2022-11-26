55604aa1fee9d2ec2852f8f7a7322f8e4bd7884b
xphinx <tjacques209@gmail.com>
2022-11-25 21:41:22 -0500

    scan fix2

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,7 @@ resource "aws_instance" "web" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
 
-   metadata_options {
-        ...
-        http_endpoint = "enabled"
-        }
+  
  
   monitoring = true
   ebs_optimized = true
